4492893bd121c2c6d65ec38ec0c98174d6f3ae11
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-07-06 12:34:35 -0300

    fix css stylesheet and added components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,16 +5,17 @@ function Footer() {
     <footer className="py-6 bg-gray-800 text-gray-50">
 	<div className="container px-6 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
 		<div className="grid grid-cols-12">
-			<div className="pb-6 col-span-full md:pb-0 md:col-span-6">
-				<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 md:justify-start">
-					<div className="flex items-center justify-center w-12 h-12 rounded-full bg-violet-400">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full text-gray-900">
-							<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-						</svg>
-					</div>
-					<span className="self-center text-2xl font-semibold">Brand name</span>
-				</a>
-			</div>
+				  <div className="pb-6 col-span-full md:pb-0 md:col-span-6">
+					  <div className="space-y-2">
+						  <h2 className="text-4xl font-bold leadi lg:text-5xl">Sign up for our newsletter</h2>
+						  <div className="dark:text-gray-400 text-xs">Be the first to know about our special offers, new product launches, and events</div>
+					  </div>
+					  <div className="mt-2">						  
+					  <input id="name" type="text" placeholder="Email Address" className="w-2/3 p-3 rounded dark:bg-gray-800" />
+					  <button type="submit" className="w-full p-3 text-sm font-bold tracki uppercase rounded dark:bg-violet-400 dark:text-gray-900">Send Message</button>
+					  </div>
+
+				  </div>
 			<div className="col-span-6 text-center md:text-left md:col-span-3">
 				<p className="pb-1 text-lg font-medium">Category</p>
 				<ul>
